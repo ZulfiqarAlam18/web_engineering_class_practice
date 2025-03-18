50a38e80b6237249021738140e80260c4197b883
ZulfiqarAlam18 <zulfi18.official@gmail.com>
2025-03-18 09:21:06 +0500

    php get and post methods

---
+++ php_work/lab_task.php
@@ -0,0 +1,12 @@
+<?php
+	$name = $_GET['name'];
+    $age = $_GET['age'];
+    $caste = $_GET['caste'];
+
+
+    echo "Name: $name <br>";
+    echo "Age: $age <br>";
+    echo "Caste: $caste <br>";
+	
+?> 
+    
